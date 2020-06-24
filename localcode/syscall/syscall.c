12b6c343971f810e8c00a4186b1298819c365343
vernon2gh <vernon2gm@gmail.com>
2020-06-24 09:15:00 +0800

    fixed: syscall package in one parameter

--- localcode/syscall/syscall.c
+++ localcode/syscall/syscall.c
@@ -5,13 +5,31 @@
 
 int main(int argc, char **argv)
 {
-	if(argc != 2)
+	int long syscall_number;
+	int parameter_number, parameter1;
+
+	if(argc < 2)
 	{
-		printf("[usage]: %s <SYSCALL_NUMBER>\n", argv[0]);
+		printf("[usage]: %s <SYSCALL_NUMBER> [parameter]\n", argv[0]);
 		return -1;
 	}
 
-	syscall(atol(argv[1]));
+	syscall_number = atol(argv[1]);
+	parameter_number = argc - 2;
+
+	switch(parameter_number)
+	{
+		case 0:
+			syscall(syscall_number);
+			break;
+		case 1:
+			parameter1 = atoi(argv[2]);
+			syscall(syscall_number, parameter1);
+			break;
+		default:
+			printf("[ERROR]: parameter number err!\n");
+			break;
+	}
 
 	return 0;
 }
