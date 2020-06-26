e779f85db90b72e28439b95b44c47148a828a4e1
vernon2gh <vernon2gm@gmail.com>
2020-06-26 15:31:18 +0800

    fixed: syscall package in one string parameter

--- localcode/syscall/syscall.c
+++ localcode/syscall/syscall.c
@@ -1,40 +1,60 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/syscall.h>
-#include <stdlib.h>
+#include <string.h>
+
+/*
+ * system call number
+ */
+#define SYSCALL_0		436
+#define SYSCALL_INT1	437
+#define SYSCALL_INT2	438
+#define SYSCALL_STR1	439
+
+void syscall_0(void)
+{
+	syscall(SYSCALL_0);
+}
+
+void syscall_int1(void)
+{
+	int p1 = 0x01;
+
+	syscall(SYSCALL_INT1, p1);
+}
+
+void syscall_int2(void)
+{
+	int p1 = 0x01;
+	int p2 = 0x02;
+
+	syscall(SYSCALL_INT2, p1, p2);
+}
+
+void syscall_str1(void)
+{
+	char p1[64] = "hello world";
+
+	syscall(SYSCALL_STR1, p1);
+	printf("kernel to user: %s\n", p1);
+}
 
 int main(int argc, char **argv)
 {
-	int long syscalln;
-	int parametern, p1, p2;
-
-	if(argc < 2)
+	if(argc != 2)
 	{
-		printf("[usage]: %s <SYSCALL_NUMBER> [parameter]\n", argv[0]);
+		printf("[usage]: %s <TYPE>\n", argv[0]);
 		return -1;
 	}
 
-	syscalln = atol(argv[1]);
-	parametern = argc - 2;
-
-	switch(parametern)
-	{
-		case 0:
-			syscall(syscalln);
-			break;
-		case 1:
-			p1 = atoi(argv[2]);
-			syscall(syscalln, p1);
-			break;
-		case 2:
-			p1 = atoi(argv[2]);
-			p2 = atoi(argv[3]);
-			syscall(syscalln, p1, p2);
-			break;
-		default:
-			printf("[ERROR]: parameter number err!\n");
-			break;
-	}
+	if(!strcmp(argv[1], "int1"))
+		syscall_int1();
+	else if(!strcmp(argv[1], "int2"))
+		syscall_int2();
+	else if(!strcmp(argv[1], "str1"))
+		syscall_str1();
+	else
+		syscall_0();
 
 	return 0;
 }
